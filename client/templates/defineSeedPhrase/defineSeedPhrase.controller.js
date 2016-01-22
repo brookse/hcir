7756d05f627f9c6b203d62a280db48e1fb377c71
Lyzzi Brooks <brookse@msoe.edu>
2016-01-22 13:37:49 -0600

    work on defining seed phrases

--- client/templates/defineSeedPhrase/defineSeedPhrase.controller.js
+++ client/templates/defineSeedPhrase/defineSeedPhrase.controller.js
@@ -1,5 +1,21 @@
 angular.module('myApp').controller('defineSeedPhraseController',
 ['$scope', '$location',
 function($scope, $location) {
-  
+  $scope.concepts = [
+    {
+      name: "concept 1",
+      seedPhrases: [
+        "one", "two", "three"
+      ]
+    }
+  ];
+
+  $scope.createConcept = function() {
+    $scope.concepts.push({
+      name: $scope.conceptForm.name,
+      seedPhrases: $scope.conceptForm.seedphrases.split(',')
+    });
+    $scope.conceptForm = {};
+    console.log('new concept array: ', $scope.concepts)
+  };
 }]);
