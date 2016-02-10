29817f83ae57649663a34aa62c3bfba228f15fe6
Lyzzi Brooks <brookse@msoe.edu>
2016-02-10 00:12:31 -0600

    quick edits of relations page

--- client/templates/defineRelations/defineRelations.controller.js
+++ client/templates/defineRelations/defineRelations.controller.js
@@ -1,5 +1,44 @@
 angular.module('myApp').controller('defineRelationsController',
 ['$scope', '$location',
 function($scope, $location) {
+  $scope.concepts  = [
+    {
+      name: "coronary artery disease",
+      seedPhrases: [
+        "coronary artery disease", "symptoms", "treatments"
+      ]
+    },
+    {
+      name: "diabetes",
+      seedPhrases: [
+        "type 1", "blood glucose", "obesity", "hypertension"
+      ]
+    },
+    {
+      name: "congestive heart failure",
+      seedPhrases: [
+        "edema", "athero schlerosis", "hypertension"
+      ]
+    },
+    {
+      name: "hepatitis",
+      seedPhrases: [
+        "liver failure", "jaundice", "alcohol"
+      ]
+    },
+    {
+      name: "cirrhosis",
+      seedPhrases: [
+        "liver damage", "hypertension"
+      ]
+    }
+  ];
+
+  $scope.currentMap = "";
+
+  $scope.showMap = function(name) {
+    console.log('name clicked:', name);
+    $scope.currentMap = name;
+  }
 
 }]);
